adf9ea67c93554d4451c07425b2d1cf96a19ddf6
Mien Tran <xuanmien13dt3@gmail.com>
2026-09-09 23:19:16 +0700

    leetcode problem: Count Commas In Range Ii

---
+++ CountCommasInRangeIi/readme.md
@@ -0,0 +1,13 @@
+## Problem
+
+https://leetcode.com/problems/count-commas-in-range-ii/
+
+```
+```
+
+## Observations
+
+## Solution
+
+# Tags
+
---
+++ CountCommasInRangeIi/solve.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long total = 0;
+        long long threshold = 1000;
+
+        while (n >= threshold) {
+            total += (n - threshold + 1);
+            threshold *= 1000; 
+        }
+        
+        return total;
+    }
+};
